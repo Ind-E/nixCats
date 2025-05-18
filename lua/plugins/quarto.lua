eff47d38cb2fb085658d7a0716066f6bb462aeb7
Indi <indiesneault@gmail.com>
2025-05-17 23:54:14 -0500

    rename

---
+++ lua/plugins/quarto.lua
@@ -0,0 +1,125 @@
+return {
+  {
+    "image.nvim",
+    after = function ()
+      require("image").setup({
+        backend = "kitty",
+        kitty_method = "normal",
+        integrations = {
+          markdown = {
+            filetypes = { "markdown", "quarto" },
+          },
+        },
+        max_width = 200,
+        max_height = 24,
+        max_height_window_percentage = math.huge,
+        max_width_window_percentage = math.huge,
+        window_overlap_clear_enabled = true,
+      })
+    end,
+  },
+  {
+    "molten-nvim",
+    ft = { "quarto", "markdown" },
+    after = function ()
+      vim.cmd.packadd("molten-nvim")
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_virt_lines_off_by_1 = true
+      vim.g.molten_auto_open_output = false
+      vim.keymap.set(
+        "n",
+        "<localleader>mi",
+        ":MoltenInit<CR>",
+        { desc = "[i]nit", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>me",
+        ":MoltenEvaluateOperator<CR>",
+        { desc = "[e]valuate operator", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>mo",
+        ":noautocmd MoltenEnterOutput<CR>",
+        { desc = "[o]pen output window", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>mr",
+        ":MoltenReevaluateCell<CR>",
+        { desc = "[r]e-eval cell", silent = true }
+      )
+      vim.keymap.set(
+        "v",
+        "<localleader>me",
+        ":<C-u>MoltenEvaluateVisual<CR>gv",
+        { desc = "[e]xecute visual selection", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>mc",
+        ":MoltenHideOutput<CR>",
+        { desc = "[c]lose output window", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>md",
+        ":MoltenDelete<CR>",
+        { desc = "[d]elete Molten cell", silent = true }
+      )
+      local runner = require("quarto.runner")
+      vim.keymap.set(
+        "n",
+        "<localleader>rc",
+        runner.run_cell,
+        { desc = "run [c]ell", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>ra",
+        runner.run_above,
+        { desc = "run cell and [a]bove", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>rA",
+        runner.run_all,
+        { desc = "run [A]ll cells", silent = true }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>rl",
+        runner.run_line,
+        { desc = "run [l]ine", silent = true }
+      )
+      vim.keymap.set(
+        "v",
+        "<localleader>r",
+        runner.run_range,
+        { desc = "[r]un visual range", silent = true }
+      )
+      vim.cmd("MoltenInit")
+    end,
+  },
+  {
+    "otter.nvim",
+    dep_of = { "quarto-nvim" },
+    after = function ()
+      require("otter").setup({})
+    end,
+  },
+  {
+    "quarto-nvim",
+    dep_of = { "molten-nvim" },
+    after = function ()
+      require("quarto").setup({
+        codeRunner = {
+          default_method = "molten",
+        },
+      })
+      require("quarto").activate()
+    end,
+  },
+}
