6e4f007882103d233cc0ad58cab85f6bfa7a7aef
Indi <indiesneault@gmail.com>
2025-08-21 21:40:33 -0700

    mini files configuration

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -57,7 +57,43 @@ return {
     "mini.files",
     event = "DeferredUIEnter",
     after = function ()
-      require("mini.files").setup()
+      require("mini.files").setup({
+        mappings = {
+          close = "<ESC>",
+        },
+      })
+
+      local minifiles_toggle = function (...)
+        if not MiniFiles.close() then
+          MiniFiles.open(...)
+        end
+      end
+
+      vim.keymap.set("n", "<leader>i", minifiles_toggle, { desc = "f[i]les" })
+
+      local show_dotfiles = true
+
+      local filter_show = function (fs_entry)
+        return true
+      end
+
+      local filter_hide = function (fs_entry)
+        return not vim.startswith(fs_entry.name, ".")
+      end
+
+      local toggle_dotfiles = function ()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        MiniFiles.refresh({ content = { filter = new_filter } })
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function (args)
+          local buf_id = args.data.buf_id
+          vim.keymap.set("n", ".", toggle_dotfiles, { buffer = buf_id })
+        end,
+      })
     end,
   },
   {
