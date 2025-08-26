c855a2f769719a7c4117ee27c2887cd563aca015
Indi <indiesneault@gmail.com>
2025-08-26 16:56:49 -0700

    remove shada keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -47,17 +47,3 @@ vim.keymap.set("n", "<leader>tn", function ()
   local number = vim.wo.number
   vim.wo.number = not number
 end, { desc = "[t]oggle [n]umber" })
-
-vim.keymap.set(
-  "n",
-  "<leader>w",
-  ":wshada! ~/tmp/shada<CR>",
-  { desc = "[w]rite shared data" }
-)
-
-vim.keymap.set(
-  "n",
-  "<leader>r",
-  ":rshada! ~/tmp/shada<CR>",
-  { desc = "[w]rite shared data" }
-)
