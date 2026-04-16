643d967b6780de911f33ebe72d0965e7daef5353
Indi Esneault <indiesneault@gmail.com>
2026-04-15 23:46:48 -0700

    treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -90,6 +90,101 @@ return {
           include_surrounding_whitespace = false,
         },
       })
+      vim.keymap.set({ "x", "o" }, "am", function ()
+        require("nvim-treesitter-textobjects.select").select_textobject(
+          "@function.outer",
+          "textobjects"
+        )
+      end)
+      vim.keymap.set({ "x", "o" }, "im", function ()
+        require("nvim-treesitter-textobjects.select").select_textobject(
+          "@function.inner",
+          "textobjects"
+        )
+      end)
+      vim.keymap.set({ "x", "o" }, "ac", function ()
+        require("nvim-treesitter-textobjects.select").select_textobject(
+          "@class.outer",
+          "textobjects"
+        )
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function ()
+        require("nvim-treesitter-textobjects.select").select_textobject(
+          "@class.inner",
+          "textobjects"
+        )
+      end)
+      -- You can also use captures from other query groups like `locals.scm`
+      vim.keymap.set({ "x", "o" }, "as", function ()
+        require("nvim-treesitter-textobjects.select").select_textobject(
+          "@local.scope",
+          "locals"
+        )
+      end)
+
+      -- movement
+
+      vim.keymap.set({ "n", "x", "o" }, "]m", function ()
+        require("nvim-treesitter-textobjects.move").goto_next_start(
+          "@function.outer",
+          "textobjects"
+        )
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]]", function ()
+        require("nvim-treesitter-textobjects.move").goto_next_start(
+          "@class.outer",
+          "textobjects"
+        )
+      end)
+      -- You can also use captures from other query groups like `locals.scm` or `folds.scm`
+      vim.keymap.set({ "n", "x", "o" }, "]s", function ()
+        require("nvim-treesitter-textobjects.move").goto_next_start(
+          "@local.scope",
+          "locals"
+        )
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]z", function ()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@fold", "folds")
+      end)
+
+      vim.keymap.set({ "n", "x", "o" }, "]M", function ()
+        require("nvim-treesitter-textobjects.move").goto_next_end(
+          "@function.outer",
+          "textobjects"
+        )
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "][", function ()
+        require("nvim-treesitter-textobjects.move").goto_next_end(
+          "@class.outer",
+          "textobjects"
+        )
+      end)
+
+      vim.keymap.set({ "n", "x", "o" }, "[m", function ()
+        require("nvim-treesitter-textobjects.move").goto_previous_start(
+          "@function.outer",
+          "textobjects"
+        )
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[[", function ()
+        require("nvim-treesitter-textobjects.move").goto_previous_start(
+          "@class.outer",
+          "textobjects"
+        )
+      end)
+
+      vim.keymap.set({ "n", "x", "o" }, "[M", function ()
+        require("nvim-treesitter-textobjects.move").goto_previous_end(
+          "@function.outer",
+          "textobjects"
+        )
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[]", function ()
+        require("nvim-treesitter-textobjects.move").goto_previous_end(
+          "@class.outer",
+          "textobjects"
+        )
+      end)
     end,
   },
 }
