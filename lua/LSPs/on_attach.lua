33fe9530ac5884ed904ef6d348785fd826f40f4e
Indi Esneault <indiesneault@gmail.com>
2026-08-21 17:53:49 -0700

    [d and ]d open hover

--- lua/LSPs/on_attach.lua
+++ lua/LSPs/on_attach.lua
@@ -52,10 +52,23 @@ return function (_, bufnr)
   nmap("gD", vim.lsp.buf.declaration, "Goto Declaration")
   nmap("gy", vim.lsp.buf.type_definition, "Goto Type Definition")
   nmap("gi", vim.lsp.buf.implementation, "Implementations")
+  nmap("]d", function ()
+    vim.diagnostic.jump({
+      count = vim.v.count1,
+      float = true,
+    })
+  end, "Jump to the next diagnostic")
+  nmap("[d", function ()
+    vim.diagnostic.jump({
+      count = -vim.v.count1,
+      float = true,
+    })
+  end, "Jump to the previous diagnostic")
   nmap("]e", function ()
     vim.diagnostic.jump({
       count = vim.v.count1,
       severity = vim.diagnostic.severity.ERROR,
+      float = true,
     })
   end, "Jump to the next error")
   nmap("[e", function ()
